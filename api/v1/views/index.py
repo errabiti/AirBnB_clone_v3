b09bbecb511f2c3d00321779ff1b97878fee1673
kharboutmanel98 <kharbout2017@gmail.com>
2024-04-29 07:57:46 +0000

    commit

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,33 +1,36 @@
-#!/usr/bin/python3
-"""index.py to connect to API"""
+sk with general routes
+    routes:
+        /status:    Returns "status": "OK" to indicate the system is up and running.
+        /stats:     Returns the total counts for all predefined classes in storage.
+'''
 from api.v1.views import app_views
-from flask import Flask, Blueprint, jsonify
+from flask import jsonify
 from models import storage
 
 
-hbnbText = {
-    "amenities": "Amenity",
-    "cities": "City",
-    "places": "Place",
-    "reviews": "Review",
-    "states": "State",
-    "users": "User"
-}
+@app_views.route("/status")
+def status():
+    '''
+        Return JSON response indicating that the system is running.
+
+    '''
+
+    return jsonify({'status': 'OK'})
 
 
-@app_views.route('/status', strict_slashes=False)
-def hbnbStatus():
-    """hbnbStatus"""
-    return jsonify({"status": "OK"})
-
-
-@app_views.route('/stats', strict_slashes=False)
-def hbnbStats():
-    """hbnbStats"""
-    return_dict = {}
-    for key, value in hbnbText.items():
-        return_dict[key] = storage.count(value)
-    return jsonify(return_dict)
-
-if __name__ == "__main__":
-    pass
+@app_views.route("/stats")
+def storage_counts():
+    '''
+        Return counts of all classes in the storage system.
+    '''
+    
+    class_counts = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(class_counts)
+    
