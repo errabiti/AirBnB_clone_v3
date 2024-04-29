d491e5e62276080e099c1f15122fae11f698ba1a
kharboutmanel98 <kharbout2017@gmail.com>
2024-04-29 13:19:49 +0200

    commit

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,4 +1,11 @@
-PI"""
+#!/usr/bin/python3
+'''
+    flask with general routes
+    routes:
+        /status:    display "status":"OK"
+        /stats:     dispaly total for all classes
+'''
+
 from api.v1.views import app_views
 from flask import Flask, Blueprint, jsonify
 from models import storage
